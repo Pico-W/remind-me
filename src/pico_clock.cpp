d8e557e45c9316574fb83af76490ef1060ea8f6f
Azaan <azaanwadhwania@yahoo.co.in>
2023-04-29 18:18:29 -0700

    add clock object

---
+++ src/pico_clock.cpp
@@ -0,0 +1,7 @@
+#include "pico_clock.h"
+
+pico_clock::pico_clock(int in_hour, int in_minute, int in_second)
+    : hour{in_hour}, minutes{in_minute}, seconds{in_second}{
+    }
+
+    
---
+++ src/pico_clock.h
@@ -0,0 +1,8 @@
+class pico_clock {
+    public:
+        int hour;
+        int minutes;
+        int seconds;
+
+        pico_clock(int hour, int minutes, int seconds);
+    };
