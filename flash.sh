dd873fc8e3122a7cd2fd8b9136e2443a25e84856
kvgagan98 <kvgagan98@gmail.com>
2023-04-29 17:47:07 -0700

    Update flash script

--- flash.sh
+++ flash.sh
@@ -1,4 +1,21 @@
 #/!/bin/bash
-sudo cp build/timer.uf2 /run/media/$USER/RPI-RP2
+MNT_PICO_DIR=/mnt/pico-w/
+filesysInfo=$(df -h | grep 'RPI')
+echo "$filesysInfo"
+
+#Prompt for filesystem that has mounted dir
+echo "Enter the fielsystem from above output..."
+read myFs
+
+echo "Filesystem with Pico-W = $myFs"
+
+if [ ! -d "$MNT_PICO_DIR" ];
+then
+    echo "Creating $MNT_PICO_DIR directory..."
+    mkdir -p $MNT_PICO_DIR
+fi
+
+sudo mount $myFs $MNT_PICO_DIR
+sudo cp build/timer.uf2 $MNT_PICO_DIR
 sudo sync
-sudo umount /run/media/$USER/RPI-RP2
+sudo umount $MNT_PICO_DIR
